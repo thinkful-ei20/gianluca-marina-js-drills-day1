f49694fd46e1c67b0187224b0f04e8c125987b62
patersog <gianluca.paterson@gmail.com>
2018-04-02 14:42:21 -0400

    Add try catch

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 function yearOfBirth(age) {
-	return 2018 - age;
+	if(age < 0) {
+		/* console.log('Age cannot be negative'); */
+		throw new Error(`Invalid Input: ${age} Age can not be negative`);
+	} else {
+		return 2018 - age;
+	}
 }
 
 function whoAmI(name, age) {
-	let yob = yearOfBirth(age);
-	console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-	console.log(`I was born in ${yob}`);
+	try {
+		let yob = yearOfBirth(age);
+		console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+		console.log(`I was born in ${yob}`);
+	} catch(e) {
+		console.log(e);
+	}
 }
 
-whoAmI('Marina', 25);
+whoAmI('Marina', -25);
